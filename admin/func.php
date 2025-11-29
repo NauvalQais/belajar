91a432ad0e69bcb7bf3f06188e5fca4fa9dbe588
Opay <127804332+NauvalQais@users.noreply.github.com>
2025-11-29 13:28:39 +0700

    lorem

---
+++ admin/func.php
@@ -0,0 +1,157 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "multi");
+// include "koneksi.php";
+
+// read
+function index($query)
+{
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+// read
+
+// create
+function store($data)
+{
+    global $conn;
+
+    $nama = ($data["nama_brand"]);
+    $serie = ($data["series_sepatu"]);
+    $harga = ($data["harga"]);
+    $jenis = ($data["jenis_sepatu"]);
+
+    // upload gambar
+    $gambar = uploads();
+    if (!$gambar) {
+        return false;
+    }
+
+    $query = "INSERT INTO products (nama_b,series_sepatu,harga,jenis,gambar)
+				VALUES
+			  ( '$nama', '$serie', '$harga', '$jenis', '$gambar')
+			";
+    mysqli_query($conn, $query);
+    // var_dump(mysqli_affected_rows($conn));
+    // die;
+    return mysqli_affected_rows($conn);
+}
+// create
+
+// uploads
+function uploads()
+{
+
+    $namaF = $_FILES['gambar']['name'];
+    $sizeF = $_FILES['gambar']['size'];
+    $error = $_FILES['gambar']['error'];
+    $tmpName = $_FILES['gambar']['tmp_name'];
+
+
+    if ($error === 4) {
+        echo "<script>
+				alert('pilih gambar terlebih dahulu!');
+			  </script>";
+        return false;
+    }
+
+    $ekstensi = ['jpg', 'jpeg', 'png'];
+    $getEks = explode('.', $namaF);
+    $getEks = strtolower(end($getEks));
+    if (!in_array($getEks, $ekstensi)) {
+        echo "<script>
+				alert('yang anda upload bukan gambar!');
+			  </script>";
+        return false;
+    }
+
+
+    if ($sizeF > 1000000) {
+        echo "<script>
+    			alert('ukuran gambar terlalu besar!');
+    		  </script>";
+        return false;
+    }
+
+
+    // generate nama gambar baru
+    $namaFileBaru = uniqid();
+    $namaFileBaru .= '.';
+    $namaFileBaru .= $getEks;
+
+    move_uploaded_file($tmpName, '../gambar/' . $namaFileBaru);
+
+    return $namaFileBaru;
+}
+// uploads
+
+
+// delete
+function destroy($id)
+{
+    global $conn;
+    $file = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM products WHERE id='$id'"));
+    unlink('../gambar/' . $file["gambar"]);
+    $delete = "DELETE FROM products WHERE id='$id'";
+    mysqli_query($conn, $delete);
+    return mysqli_affected_rows($conn);
+    // mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id");
+    // return mysqli_affected_rows($conn);
+}
+// delete
+
+// update
+function update($data)
+{
+    global $conn;
+
+    $id = $data["id"];
+    $nama = ($data["nama_brand"]);
+    $serie = ($data["series_sepatu"]);
+    $harga = ($data["harga"]);
+    $jenis = ($data["jenis_sepatu"]);
+    $gambarLama = ($data["gambarLama"]);
+
+    // cek apakah user pilih gambar baru atau tidak
+    if ($_FILES['gambar']['error'] === 4) {
+        $gambar = $gambarLama;
+    } else {
+        $gambarLama = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM products WHERE id='$id'"));
+        unlink('../gambar/' . $gambarLama["gambar"]);
+
+        $gambar = uploads();
+    }
+
+
+    $query = "UPDATE products SET
+				nama_b = '$nama',
+				series_sepatu = '$serie',
+				harga = '$harga',
+				jenis = '$jenis',
+				gambar = '$gambar'
+			  WHERE id = $id
+			";
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+// update
+
+// search
+function search($keyword)
+{
+    $query = "SELECT * FROM products
+				WHERE
+			  nama_b LIKE '%$keyword%' OR
+			  series_sepatu LIKE '%$keyword%' OR
+			  jenis LIKE '%$keyword%' OR
+			  harga LIKE '%$keyword%'
+			";
+    return index($query);
+}
+// search
